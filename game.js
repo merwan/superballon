0af7e281e47e405b6217a23797884f6014b6105d
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-12-24 23:46:13 +0100

    refactor World

--- game.js
+++ game.js
@@ -1,58 +1,68 @@
 $(document).ready(function() {
-    var canevas = document.getElementById("drawing")
-    var ctx = canevas.getContext("2d")
-
     function Cible(couleur, taille, posX, posY) {
         this.couleur = couleur;
         this.hauteur = taille;
         this.largeur = taille;
         this.positionX = posX;
         this.positionY = posY;
+        
         this.contient = function(x, y) {
             return (x >= this.positionX && x <= (this.positionX + this.largeur)) && (y >= this.positionY && y <= (this.positionY + this.hauteur));
         };
     }
-    
-    var cibles = [
+
+    function World(canevas, cibles) {
+        this.canvas = $(canevas);
+        this.ctx = canevas.getContext("2d");
+        this.cibles = cibles;
+
+        this.dessinerCanevas = function() {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            for (cible in this.cibles) {
+                var cibleCourante = this.cibles[cible];
+                this.dessinerCible(cibleCourante);
+            }
+        };
+        this.dessinerCible = function(cible) {
+            this.ctx.fillStyle = cible.couleur;
+            this.ctx.fillRect(cible.positionX, cible.positionY, cible.largeur, cible.hauteur);
+        }
+        this.run = function() {
+            this.dessinerCanevas();
+        }
+
+        this.canvas.mousedown(function(evenement) {
+            var sourisX,
+                sourisY;
+            
+            sourisX = evenement.clientX;
+            sourisY = evenement.clientY;
+
+            sourisX -= this.offsetLeft;
+            sourisY -= this.offsetTop;
+
+            for (cible in cibles) {
+                var cible1 = cibles[cible];
+                alert("posY:" + cible1.positionY + " sourisY:" + sourisY);
+                if (cible1.contient(sourisX, sourisY)) {
+                    alert("Bravo !");
+                    cibles.splice(cible, 1);
+                    // TODO: comment appeler la méthode de World?
+                    dessinerCanevas();
+                    return;
+                }
+            }
+            alert("Perdu hahaha ! Recommencez.");
+        });
+    }
+
+    var canevas = document.getElementById("drawing");
+    var targets = [
         new Cible('#f00', 80, 350, 200),
         new Cible('#0f0', 80, 405, 100),
         new Cible('#e4e', 100, 200, 100)
     ];
-
-    dessinerCanevas(ctx);
-
-    $("#drawing").mousedown(function(evenement) {
-        var sourisX,
-            sourisY;
-        
-        sourisX = evenement.clientX;
-        sourisY = evenement.clientY;
-        
-        sourisX -= this.offsetLeft;
-        sourisY -= this.offsetTop;
-
-        for (cible in cibles) {
-            var cible1 = cibles[cible];
-            if (cible1.contient(sourisX, sourisY)) {
-                alert("Bravo !");
-                cibles.splice(cible, 1);
-                dessinerCanevas(ctx);
-                return;
-            }
-        }
-        alert("Perdu hahaha ! Recommencez.");
-    });
     
-    function dessinerCanevas(ctx) {
-        ctx.clearRect(0, 0, 768, 400);
-        for (cible in cibles) {
-            var cibleCourante = cibles[cible];
-            dessinerCible(ctx, cibleCourante);
-        }
-    }
-
-    function dessinerCible(ctx, cible) {
-        ctx.fillStyle = cible.couleur;
-        ctx.fillRect(cible.positionX, cible.positionY, cible.largeur, cible.hauteur);
-    }
+    var world = new World(canevas, targets);
+    world.run();
 });
